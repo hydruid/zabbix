ccc2a863a8f6923e681592210e77bf1ae6abcde8
Hydruid <trey.hunsucker@gmail.com>
2020-04-16 09:53:06 -0500

    Thu Apr 16 09:53:06 CDT 2020

--- 4.0/update.sh
+++ 4.0/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Refresh repo's, Install Updates, and Remove stale packages
+#Refresh repo's, Install Updates, and Remove stale packages without prompting
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
