06b6333f113217cc7cefb4f3aba4c0269a5b978b
Hydruid <trey.hunsucker@gmail.com>
2020-05-05 07:10:30 -0500

    Tue May  5 07:10:30 CDT 2020

--- API/example.pl
+++ API/example.pl
@@ -6,6 +6,7 @@ use Zabbix::Tiny;
 use IO::Socket::SSL;
 use Data::Dumper;
 
+#Variables
 my $url      = $ARGV[0];
 my $username = $ARGV[1];
 my $password = $ARGV[2];
@@ -21,19 +22,16 @@ my $zabbix = Zabbix::Tiny->new(
 # Send a JSON-RPC request to the zabbix server (host.get in this case)
 my $hosts = $zabbix->do(
     'host.get',  # First argument is the Zabbix API method
-    output    => [qw(hostid name host)], # Remaining paramters to 'do' are the params for the zabbix method.
-	monitored_hosts	=> 1,
-    limit		=> 1,
+    output => [qw(hostid name host)], # Remaining paramters to 'do' are the params for the zabbix method.
+    monitored_hosts => 1,
+    limit => 1,
 );
 
+#Print output
 print "JSON request:\n" . $zabbix->json_request . "\n\n";	# Print the json data sent in the last request.
 print "JSON response:\n" . $zabbix->json_response . "\n\n";	# Print the json data received in the last response.
-print "Auth is: ". $zabbix->auth . "\n";
 
-print "\$zabbix->last_response:\n";
+#Other Print output options
+#print "Auth is: ". $zabbix->auth . "\n";
+#print "\$zabbix->last_response:\n";
 #print Dumper $zabbix->last_response;
-
-
-print "JSON request:\n" . $zabbix->json_request . "\n"; 
-# Very verbose:
-#print Dumper $zabbix->post_response;
