f90764977871f31fd368251d813da7b5e9c8a868
hydruid <trey.hunsucker@gmail.com>
2020-10-19 15:47:58 -0500

    Added additional repo refresh

--- 5.0/update.sh
+++ 5.0/update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#Refresh repo's, Install Updates, and Remove stale packages without prompting
+#Refresh repos, Install Updates, and Remove stale packages without prompting
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
+
+#Refresh repos one last time
+sudo apt-get update
 
 #Stop script
 exit 0
